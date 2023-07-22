8a32d9278d3d29340dec2d314f7f4148bee5503e
Michael Alikbarov <michael.alikbarov@yandex.ru>
2023-07-22 22:37:43 +0600

    вернуть настройки разработчика

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -1,5 +1,5 @@
 import checkResponse from "./checkResponse";
-export const BASE_URL = "https://api.mesto.almichael.nomoredomains.xyz";
+export const BASE_URL = "http://localhost:5000";
 
 function request(url, options) {
   return fetch(`${BASE_URL}/${url}`, options).then(checkResponse);
